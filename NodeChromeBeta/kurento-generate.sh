c76f1ae38e1b252b0e31c873eb9ffa79be7a08fe
Fede Diaz <nordri@gmail.com>
2017-06-26 14:52:08 +0200

    Update kurente-generate

--- NodeChromeBeta/kurento-generate.sh
+++ NodeChromeBeta/kurento-generate.sh
@@ -2,8 +2,11 @@
 
 echo "##################### EXECUTE: kurento-generate.sh #####################"
 
+cat ../NodeChrome/Dockerfile.txt >> ./Dockerfile
 sed -i 's/google-chrome-stable/google-chrome-beta/' ./Dockerfile
 sed -i 's|COPY chrome_launcher.sh /opt/google/chrome/google-chrome|COPY chrome_launcher.sh /opt/google/chrome-beta/google-chrome|' ./Dockerfile
 sed -i 's|RUN chmod +x /opt/google/chrome/google-chrome|RUN chmod +x /opt/google/chrome-beta/google-chrome|' ./Dockerfile
+cp ../NodeChrome/generate_config .
+cp ../NodeChrome/chrome_launcher.sh .
 sed -i 's|export CHROME_VERSION_EXTRA="stable"|export CHROME_VERSION_EXTRA="beta"|' ./chrome_launcher.sh
 
