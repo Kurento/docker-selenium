37c70f9ce492bb06eda85556d81ef0c74055638a
Fede Diaz <nordri@gmail.com>
2017-06-27 14:55:46 +0200

    No IPRoute included

--- NodeFirefoxDebugDnat/kurento-generate.sh
+++ NodeFirefoxDebugDnat/kurento-generate.sh
@@ -14,8 +14,11 @@ cp entry_point.sh entry_point_selenium.sh
 cat > entry_point.sh <<-EOF
 #!/bin/bash
 cd /opt/bin/
+apt-get install -y iproute
 ./entry_point_dnat.sh
 EOF
+
+
 
 sed -i 's/node-base/node-firefox/' Dockerfile
 sed -i 's/COPY entry_point.sh \/opt\/bin\/entry_point.sh/COPY entry_point.sh entry_point_selenium.sh entry_point_dnat.sh \/opt\/bin\//' Dockerfile
