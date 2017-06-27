29280ead55041f08ecd16d419e8c51eb185735d7
Fede Diaz <nordri@gmail.com>
2017-06-27 16:28:09 +0200

    NodeFirefoxBetaDebugDnat

---
+++ NodeFirefoxBetaDebugDnat/kurento-generate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+echo "##################### EXECUTE: kurento-generate.sh #####################"
+
+VERSION=$1
+NAMESPACE=$2
+AUTHORS=$3
+
+cd ../NodeDebug && ./generate.sh NodeFirefoxBetaDebugDnat node-firefox-beta-debug Firefox $1 $2 $3
+
+cd ../NodeFirefoxBetaDebugDnat
+cp entry_point.sh entry_point_selenium.sh
+
+cat > entry_point.sh <<-EOF
+#!/bin/bash
+cd /opt/bin/
+./entry_point_dnat.sh
+EOF
+
+sed -i 's/COPY entry_point.sh \/opt\/bin\/entry_point.sh/COPY entry_point.sh entry_point_selenium.sh entry_point_dnat.sh \/opt\/bin\//' Dockerfile
+
