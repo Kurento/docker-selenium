4582d5e5545996371620f73080653bd18ba8d236
Fede Diaz <nordri@gmail.com>
2017-06-28 10:51:11 +0200

    Update chrome binary path for beta and dev

--- NodeChromeBeta/kurento-generate.sh
+++ NodeChromeBeta/kurento-generate.sh
@@ -7,6 +7,7 @@ sed -i 's/google-chrome-stable/google-chrome-beta/' ./Dockerfile
 sed -i 's|COPY chrome_launcher.sh /opt/google/chrome/google-chrome|COPY chrome_launcher.sh /opt/google/chrome-beta/google-chrome|' ./Dockerfile
 sed -i 's|RUN chmod +x /opt/google/chrome/google-chrome|RUN chmod +x /opt/google/chrome-beta/google-chrome|' ./Dockerfile
 cp ../NodeChrome/generate_config .
+sed -i 's|/opt/google/chrome|/opt/google/chrome-beta|' ./generate_config
 cp ../NodeChrome/chrome_launcher.sh .
 sed -i 's|export CHROME_VERSION_EXTRA="stable"|export CHROME_VERSION_EXTRA="beta"|' ./chrome_launcher.sh
 
